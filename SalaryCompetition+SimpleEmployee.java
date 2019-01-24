59321d88d86f2090da1e862684384e92a86376bd
meep <44884309+sleepymeep@users.noreply.github.com>
2019-01-24 17:26:17 +0800

    Update SalaryCompetition+SimpleEmployee.java

--- SalaryCompetition+SimpleEmployee.java
+++ SalaryCompetition+SimpleEmployee.java
@@ -1,60 +1,79 @@
 package salarycompetition;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 import java.text.DecimalFormat;
 
 public class SalaryCompetition {
-
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        DecimalFormat df2 = new DecimalFormat("#.00");
+    
+    public static double percent = 0.005;
+    public static double salary = 1500;
+    public static double bonus = 0;
+    public static double netweekly = 0;
+    
+    public static void main(String[] args) {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner br = new Scanner(System.in);
+        //DecimalFormat df2 = new DecimalFormat("#.00");
         
         SimpleEmployee se1 = new SimpleEmployee();
         SimpleEmployee se2 = new SimpleEmployee();
         
         se1.setName("Jon");
         se1.setWeeklyGross(1500);
-        //se1.setClassifications('A'); (optional cause it dosen't affect the results)
         
         se2.setName("Ann");
-        se2.setWeeklyGross(1500);
-        //se2.setClassifications('C'); (optional cause it dosen't affect the results)
+        se2.setWeeklyGross(1500); 
         
-        System.out.print("Enter Namme: ");
-        String name = br.readLine();
+        System.out.print("Enter Name: ");
+        String name = br.nextLine();
+        while(name.isEmpty()){
+            System.out.println("Please Enter Employee Name: "); //repeats 
+            name = br.nextLine();
+        }
         System.out.print("Enter Weekly Gross: ");
-        double weeklySales = Double.parseDouble(br.readLine());
+        double gross = br.nextDouble();
+        bonus = gross * percent;
+        DecimalFormat df = new DecimalFormat("######0.00");
+        if (salary<1500){
+            System.out.println("Please Enter a salary above 1500:");  //repeats
+             gross = br.nextDouble();
+        }else{
+            System.out.println("Name: \t\t" + name);
+            System.out.println("Weekly Gross: \t\t" + df.format(gross));
         
         System.out.println("\n****** Snow Enterprises *****");
         System.out.println("Employee Name:\t\t" + name);
-        System.out.println("Gross Salary:\t\t" + df2.format(((weeklySales))));
-        System.out.println("Bonus:\t\t\t" + df2.format(((8.5/100)*weeklySales)));
-        System.out.println("Net Weekly Salary:\t" + df2.format((weeklySales)));
-        System.out.println("Classification:\t\t" + classify((weeklySales)));
-    }
-    public double netweekly(double weeklySales){
-        double netweeklySales = ((8.5/100)*weeklySales)+1500;
-        return netweeklySales;
-    }
-    public static char classify(double netweeklySales){
-        char employeeClass = ' ';
-        char[] classification = {'A', 'B', 'C', 'D', 'E', 'F'};
-        int[] x = {1500, 2000, 2500, 3000, 3500, 4000};
-        int[] y = {1999, 2499, 2999, 3499, 3999, 1000000};
-        for(int i = 0; i < classification.length; i++){
-            if(netweeklySales >= x[i] && netweeklySales <= y[i]){
-                employeeClass = classification[i];
-            }
+        System.out.println("Gross Salary:\t\t" + df.format(gross));
+        System.out.println("Bonus:\t\t\t" + df.format(bonus));
+        netweekly();
+        classify();
         }
-        return employeeClass;
+    }
+    public static void netweekly(){
+        DecimalFormat df = new DecimalFormat("######0.00"); //its important to copy and paste that to avoid Errors
+        netweekly = bonus + 1500;
+        System.out.println("Net Weekly Salary:\t" + df.format(netweekly));
+    }
+    public static void classify(){
+            if(netweekly>1500 && netweekly<1999){
+                System.out.println("Classification:\t\t A");
+            }else if(netweekly>2000 && netweekly<2499){
+                System.out.println("Classification:\t\t B");
+            }else if(netweekly>2500 && netweekly<2999){
+                System.out.println("Classification:\t\t C");
+            }else if(netweekly>3000 && netweekly<3499){
+                System.out.println("Classification:\t\t D");
+            }else if(netweekly>3500 && netweekly<3999){
+                System.out.println("Classification:\t\t E");
+            }else if(netweekly>4000){
+                System.out.println("Classification:\t\t F");
+            }
     }
 }
 
 //create a class along with salarycompetition to make a class for the employees to support the salarycompetition
-package salarycompetition;
 
+package salarycompetition;
 public class SimpleEmployee {
     
     String Name;
@@ -80,9 +99,9 @@ public class SimpleEmployee {
                 return this.getClassifications();
             }
         
-        //public void setFixedSalary(float salary){
-        //    this.FixedSalary = salary;
-        //}    
+        public void setFixedSalary(float salary){
+            this.FixedSalary = salary;
+        }    
             public float getFixedSalary(){
                 return this.FixedSalary;
             }
@@ -93,37 +112,25 @@ public class SimpleEmployee {
             public double getWeeklyGross(){
                 return this.WeeklyGross;
             }
-            
+        
+        public void setBonusRate(double bonus){
+            this.BonusRate = bonus;
+        }
              public double getBonusRate(){
                  return this.BonusRate;
             }    
 }
 
 
-
 Output:
-Enter Namme: Jon
+Enter Name: Jon
 Enter Weekly Gross: 1500
+Name: 		Jon
+Weekly Gross: 		1500.00
 
 ****** Snow Enterprises *****
 Employee Name:		Jon
 Gross Salary:		1500.00
-Bonus:			127.50
-Net Weekly Salary:	1500.00
-Classification:		D
-
-//if the Weekly Gross is above 1500, there's a chance for the classification to raise up
-//however if the number is below 150 for example 120 there won't be a classification to rank
-
-Enter Namme: Ann
-Enter Weekly Gross: 120
-
-****** Snow Enterprises *****
-Employee Name:		Ann
-Gross Salary:		120.00
-Bonus:			10.20
-Net Weekly Salary:	120.00
-Classification:		
-
-//and if the Weekly Gross is 0, it will be still zero with no classification
-//chances of getting a classification A, B, or C would be nearly impossible cause the user will get an D, E, or F classification only
+Bonus:			    7.50
+Net Weekly Salary:	1507.50
+Classification:		 A
